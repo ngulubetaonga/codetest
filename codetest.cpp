79afbe066f2eb876f5186f89e891c47d99f8a394
ngulubetaonga <154499489+ngulubetaonga@users.noreply.github.com>
2023-12-21 16:59:59 +0200

    File Uploaded as part of a a code test

---
+++ codetest.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <string>
+int main() {
+    int start, end;
+    std:: cout << "enter the starting number:";
+    std:: cin >> start;
+    std:: cout << "enter the end number:";
+    std:: cin >> end;
+
+    for (int i = start; 1 <= end; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "rayroy" << std::endl;
+        }
+        else if (i % 3 == 0) {
+            std::cout << "ray" std:: endl;
+        }
+        else if (i % 5 == 0) {
+            std::cout << "ry" std: : endl;
+        }
+        else {
+            std::cout << i << std: : endl;
+     }
